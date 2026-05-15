acabf0dab67060323d240ca531ca67c081873228
speakerchef <sohannair25@gmail.com>
2026-05-16 01:38:43 +0800

    feat: Add on_hover color state change for import/export buttons

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use bevy::prelude::*;
+use bevy::{picking::hover::Hovered, prelude::*};
 mod palette;
 
 fn main() {
@@ -6,13 +6,106 @@ fn main() {
         .add_plugins(DefaultPlugins.set(WindowPlugin {
             primary_window: Some(Window {
                 title: "Polarity".into(),
-                mode: bevy::window::WindowMode::BorderlessFullscreen(MonitorSelection::Primary),
+                // mode: bevy::window::WindowMode::BorderlessFullscreen(MonitorSelection::Primary),
                 ..Default::default()
             }),
             ..Default::default()
         }))
         .add_systems(Startup, setup)
         .run();
+}
+
+fn button_on_hover(
+    event: On<Pointer<Over>>,
+    mut q_bgcol: Query<&mut BackgroundColor, With<Button>>,
+    mut q_textcol: Query<&mut TextColor>,
+    q_children: Query<&Children>,
+) {
+    let target = event.event_target();
+    if let Ok(mut bgcol) = q_bgcol.get_mut(target) {
+        bgcol.0 = palette::BRIGHT;
+        if let Ok(children) = q_children.get(target) {
+            for child in children {
+                if let Ok(mut c) = q_textcol.get_mut(*child) {
+                    c.0 = palette::INK;
+                }
+            }
+        }
+    }
+}
+fn button_on_leave(
+    event: On<Pointer<Out>>,
+    mut q_bgcol: Query<&mut BackgroundColor, With<Button>>,
+    mut q_textcol: Query<&mut TextColor>,
+    q_children: Query<&Children>,
+) {
+    let target = event.event_target();
+    if let Ok(mut bgcol) = q_bgcol.get_mut(target) {
+        bgcol.0 = palette::BG_MED;
+        if let Ok(children) = q_children.get(target) {
+            for child in children {
+                if let Ok(mut c) = q_textcol.get_mut(*child) {
+                    c.0 = palette::TEXT;
+                }
+            }
+        }
+    }
+}
+
+fn spawn_file_handle_button(
+    parent: &mut ChildSpawnerCommands,
+    icon_font: Handle<Font>,
+    text_font: Handle<Font>,
+    button_text: &str,
+    arrow_glyph: &str,
+    // on_click: fn(parent: &mut ChildSpawnerCommands),
+    add_border: bool,
+) {
+    parent
+        .spawn((
+            Button,
+            Node {
+                display: Display::Flex,
+                justify_content: JustifyContent::Center,
+                align_items: AlignItems::Center,
+                height: percent(100.),
+                width: percent(50.),
+                border: if add_border {
+                    UiRect {
+                        right: px(palette::FRAME_WIDTH),
+                        ..Default::default()
+                    }
+                } else {
+                    UiRect::default()
+                },
+                ..Default::default()
+            },
+            BackgroundColor(palette::BG_MED),
+            BorderColor::all(palette::BORDER),
+        ))
+        .with_children(|parent| {
+            parent.spawn((
+                Text::new(arrow_glyph),
+                TextColor(palette::TEXT),
+                TextFont {
+                    font: icon_font.clone(),
+                    font_size: palette::font_size::ICON,
+                    ..Default::default()
+                },
+            ));
+            parent.spawn((
+                Text::new(button_text),
+                TextColor(palette::TEXT),
+                TextFont {
+                    font: text_font.clone(),
+                    font_size: palette::font_size::BRAND,
+                    weight: FontWeight(palette::font_weight::HEAVY),
+                    ..Default::default()
+                },
+            ));
+        })
+        .observe(button_on_hover)
+        .observe(button_on_leave);
 }
 
 fn setup(mut commands: Commands, asset_server: ResMut<AssetServer>) {
@@ -95,80 +188,22 @@ fn setup(mut commands: Commands, asset_server: ResMut<AssetServer>) {
                                     BorderColor::all(palette::BORDER),
                                 ))
                                 .with_children(|parent| {
-                                    // Import button
-                                    parent
-                                        .spawn((
-                                            Node {
-                                                display: Display::Flex,
-                                                justify_content: JustifyContent::Center,
-                                                align_items: AlignItems::Center,
-                                                height: percent(100.),
-                                                width: percent(50.),
-                                                border: UiRect {
-                                                    right: px(palette::FRAME_WIDTH),
-                                                    ..Default::default()
-                                                },
-                                                ..Default::default()
-                                            },
-                                            BackgroundColor(palette::BG_MED),
-                                            BorderColor::all(palette::BORDER),
-                                        ))
-                                        .with_children(|parent| {
-                                            parent.spawn((
-                                                Text::new("\u{e5db}"),
-                                                TextColor(palette::TEXT),
-                                                TextFont {
-                                                    font: icon_font.clone(),
-                                                    font_size: palette::font_size::ICON,
-                                                    ..Default::default()
-                                                },
-                                            ));
-                                            parent.spawn((
-                                                Text::new("IMPORT"),
-                                                TextColor(palette::TEXT),
-                                                TextFont {
-                                                    font: inter.clone(),
-                                                    font_size: palette::font_size::BRAND,
-                                                    weight: FontWeight(palette::font_weight::HEAVY),
-                                                    ..Default::default()
-                                                },
-                                            ));
-                                        });
-                                    // Export button
-                                    parent
-                                        .spawn((
-                                            Button,
-                                            Node {
-                                                display: Display::Flex,
-                                                justify_content: JustifyContent::Center,
-                                                align_items: AlignItems::Center,
-                                                height: percent(100.),
-                                                width: percent(50.),
-                                                ..Default::default()
-                                            },
-                                            BackgroundColor(palette::BG_MED),
-                                        ))
-                                        .with_children(|parent| {
-                                            parent.spawn((
-                                                Text::new("\u{e5d8}"),
-                                                TextColor(palette::TEXT),
-                                                TextFont {
-                                                    font: icon_font.clone(),
-                                                    font_size: palette::font_size::ICON,
-                                                    ..Default::default()
-                                                },
-                                            ));
-                                            parent.spawn((
-                                                Text::new("EXPORT"),
-                                                TextColor(palette::TEXT),
-                                                TextFont {
-                                                    font: inter.clone(),
-                                                    font_size: palette::font_size::BRAND,
-                                                    weight: FontWeight(palette::font_weight::HEAVY),
-                                                    ..Default::default()
-                                                },
-                                            ));
-                                        });
+                                    spawn_file_handle_button(
+                                        parent,
+                                        icon_font.clone(),
+                                        inter.clone(),
+                                        "IMPORT",
+                                        "\u{e5db}",
+                                        true,
+                                    );
+                                    spawn_file_handle_button(
+                                        parent,
+                                        icon_font.clone(),
+                                        inter.clone(),
+                                        "EXPORT",
+                                        "\u{e5d8}",
+                                        false,
+                                    );
                                 });
                             // Inner div for control panel
                             parent.spawn((
